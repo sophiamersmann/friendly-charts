b3db7a38f81f4b78f7c210b787be83b598178ab7
sophiamersmann <sophia.mersmann1@gmail.com>
2022-09-04 11:28:45 +0200

    delete site

--- site/README.md
+++
@@ -1,3 +0,0 @@
-# Friendly charts
-
-Exploration of screen reader friendly charts with minimal effort
--- site/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-/** Constructs a translate(x,y) string for use with the `transform` CSS property */
-export function translate(xy: number[]) {
-	return `translate(${xy})`;
-}
-
-/** Appends 'px' to a given number */
-export function px(n: number) {
-	return `${n}px`;
-}
